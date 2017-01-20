b392eef33ca7b8656277984bb5e9a166e998b2eb
Michael Gerdemann <michael.gerdemann@avency.de>
2017-01-20 08:41:37 +0100

    TASK: Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Gerdemann.VRView
+===========================
+
+Neos plugin for Google VR View
+
+How-To:
+-------
+
+* Install the package to ``Packages/Plugin/Gerdemann.VRView`` (e.g. via ``composer require gerdemann/vrview:~1.0``)
+* Insert the new Element ``VR View`` and choose your 360° image or video
+* Publish all changes
+
+Now you should see the Google VR View in the frontend.
---
+++ Resources/Public/JavaScript/VRView.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', onVrViewLoad);
+
+function onVrViewLoad() {
+    var vrViewElements = document.getElementsByClassName('gerdemann-vrview');
+    for (var i = 0; i < vrViewElements.length; i++) {
+        var options = {
+            is_stereo: vrViewElements[i].dataset.isstereo,
+            width: vrViewElements[i].dataset.width,
+            height: vrViewElements[i].dataset.height,
+            default_yaw: vrViewElements[i].dataset.defaultyaw
+        }
+        if (typeof vrViewElements[i].dataset.image != 'undefined') {
+            options.image = vrViewElements[i].dataset.image;
+        } else if (typeof vrViewElements[i].dataset.video != 'undefined') {
+            options.video = vrViewElements[i].dataset.video;
+        }
+
+        var vrView = new VRView.Player(
+            '#' + vrViewElements[i].id,
+            options
+        );
+    }
+}
