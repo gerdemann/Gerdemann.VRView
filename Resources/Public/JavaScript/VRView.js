1a7ecf97389d896f74a05d777f1b02fb8cb730a9
Michael Gerdemann <michael@gerdemann.me>
2017-01-21 11:29:48 +0100

    TASK: Activate VR View in backend

--- Resources/Public/JavaScript/VRView.js
+++ Resources/Public/JavaScript/VRView.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', onVrViewLoad);
 
+if (typeof document.addEventListener === 'function') {
+    document.addEventListener('Neos.PageLoaded', onVrViewLoad, false);
+}
+
 function onVrViewLoad() {
     var vrViewElements = document.getElementsByClassName('gerdemann-vrview');
     for (var i = 0; i < vrViewElements.length; i++) {
@@ -8,7 +12,8 @@ function onVrViewLoad() {
             width: vrViewElements[i].dataset.width,
             height: vrViewElements[i].dataset.height,
             default_yaw: vrViewElements[i].dataset.defaultyaw
-        }
+        };
+
         if (typeof vrViewElements[i].dataset.image != 'undefined') {
             options.image = vrViewElements[i].dataset.image;
         } else if (typeof vrViewElements[i].dataset.video != 'undefined') {
